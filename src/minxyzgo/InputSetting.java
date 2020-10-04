a5e7c245dd5029f5230037b26c958c9ec74abb9f
Minxyzgo <1225327866@qq.com>
2020-10-04 18:43:53 +0800

    Fixed bug

--- src/minxyzgo/InputSetting.java
+++ src/minxyzgo/InputSetting.java
@@ -1,9 +1,13 @@
 package minxyzgo;
 
+import arc.*;
 import static mindustry.Vars.*;
+import static mindustry.game.EventType.*;
 
 public class InputSetting {
 	public static void set(){
-		control.setInput(mobile ? new EXMobileInput() : new EXDesktopInput());
+	    Events.on(WorldLoadEvent.class, event -> {
+	    	control.setInput(mobile ? new EXMobileInput() : new EXDesktopInput());
+	    });
 	}
 }
